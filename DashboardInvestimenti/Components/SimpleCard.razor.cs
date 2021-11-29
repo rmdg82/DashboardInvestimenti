15e3aca5653b9df43797e3ad7eff5e20c09088cb
Romeo di Gregorio <r.digregorio@crif.com>
2021-11-29 06:06:17 +0100

    Add new component for simple cards

---
+++ DashboardInvestimenti/Components/SimpleCard.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashboardInvestimenti.Components
+{
+    public partial class SimpleCard
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public string Color { get; set; }
+
+        [Parameter]
+        public string Value { get; set; }
+    }
+}
